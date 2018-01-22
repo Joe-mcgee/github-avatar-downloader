5a8a7da64a9a29fe2690c1c9faaf2a9911583cc1
Taylor <thulsmans.2994@me.com>
2018-01-22 22:24:50 +0000

    avatar_url functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,10 @@
 const request = require('request');
-const secrets = require('secrets.js')
+const secrets = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
-function getRepoContributiors(repoOwner, repoName, cb) {
+function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -14,13 +14,17 @@ function getRepoContributiors(repoOwner, repoName, cb) {
 
   };
   request(options, function(err, res, body) {
-    cb(err, body);
+    let parsed = JSON.parse(body)
+    let avaURLS = []
+    parsed.forEach(function(contributor) {
+      avaURLS.push(contributor['avatar_url'])
+    })
+    cb(err, body, avaURLS);
   });
 }
 
 
-getRepoContributiors('jquery', 'jquery', function(err, result){
-
+getRepoContributors('jquery', 'jquery', function(err, result, target){
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log('URLS to Avatars: ', target)
 });
