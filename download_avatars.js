bbc0d3a3ef61b25b19c06d8892607361430a1ba1
Taylor <thulsmans.2994@me.com>
2018-01-22 22:11:28 +0000

    token

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,26 @@
 const request = require('request');
+const secrets = require('secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributiors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
+    }
+
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributiors('jquery', 'jquery', function(err, result){
+
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
