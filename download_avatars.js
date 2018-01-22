d15b3590b9fd2f5969870903f4c4f7bfd8507c15
Taylor <thulsmans.2994@me.com>
2018-01-22 22:57:25 +0000

    connected functions

--- download_avatars.js
+++ download_avatars.js
@@ -15,18 +15,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   };
   request(options, function(err, res, body) {
-    let parsed = JSON.parse(body)
-    let avaURLS = []
-    parsed.forEach(function(contributor) {
-      avaURLS.push(contributor['avatar_url'])
-    })
-    cb(err, body, avaURLS);
+
+    cb(err, body);
   });
 }
 
 
-getRepoContributors('jquery', 'jquery', function(err, result, target){
-  return target
+getRepoContributors('jquery', 'jquery', function(err, result){
+  let parsed = JSON.parse(result);
+  let loginAvatarUrl = {};
+  parsed.forEach(function(contributor) {
+      loginAvatarUrl[contributor['login']] = contributor['avatar_url'];
+    });
+
+  for (login in loginAvatarUrl) {
+    /*try {
+      fs.writeFileSync('/avatars/' + login + '.jpg')
+    } catch (e) {
+      console.log('cannot write file', e)
+    }*/
+    downloadImageByURL(loginAvatarUrl[login], './avatars/' + login + '.jpg');
+  }
 });
 
 
