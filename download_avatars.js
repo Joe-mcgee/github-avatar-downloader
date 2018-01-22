120417e49713e8dc441079dca0f6c5a6bc06e4ea
Taylor <thulsmans.2994@me.com>
2018-01-22 23:51:48 +0000

    error handling for no target folder

--- download_avatars.js
+++ download_avatars.js
@@ -5,14 +5,33 @@ const secrets = require('./secrets.js');
 const fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+
 //writes jpgs to an avatars folder
 function downloadImageByURL(url, filePath) {
+  if (!fs.existsSync('./avatars')) {
+    fs.mkdirSync('./avatars');
+  }
+
   request.get(url)
     .on('error', function(err) {
       console.log(err);
     })
     .pipe(fs.createWriteStream(filePath));
 
+}
+// callback which  handles html to json to relevant data
+function handleFilter(err, result) {
+  let parsed = JSON.parse(result);
+  let loginAvatarUrl = {};
+  //cretes {'login name': 'avatar_url'} object
+  parsed.forEach(function(contributor) {
+    loginAvatarUrl[contributor['login']] = contributor['avatar_url'];
+  });
+  for (login in loginAvatarUrl) {
+    downloadImageByURL(loginAvatarUrl[login], './avatars/' + login + '.jpg');
+  }
 }
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -25,23 +44,14 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   };
   request(options, function(err, res, body) {
-    //call back function starting at line 34
     cb(err, body);
   });
 }
 // if input is defined, execute function
 if (typeof owner !== 'undefined' && typeof repo !== 'undefined') {
-  getRepoContributors(owner, repo, function(err, result) {
-    let parsed = JSON.parse(result);
-    let loginAvatarUrl = {};
-    //cretes {'login name': 'avatar_url'} object
-    parsed.forEach(function(contributor) {
-      loginAvatarUrl[contributor['login']] = contributor['avatar_url'];
-    });
-    for (login in loginAvatarUrl) {
-      downloadImageByURL(loginAvatarUrl[login], './avatars/' + login + '.jpg');
-    }
-  });
+  getRepoContributors(owner, repo, handleFilter);
 } else {
-  console.log('please enter the owner and in the cmd line');
+  console.log('please enter the owner and the repo in the cmd line');
 }
+
+
